87d319df09bf58192ebb478fd563fe23e29eb7f0
aarsenych <arsenycholexandra@gmail.com>
2018-06-07 17:11:59 +0300

    last update

---
+++ src/com/gmail/arsenycholexandra/Circle.java
@@ -0,0 +1,18 @@
+package com.gmail.arsenycholexandra;
+import java.util.Scanner;
+
+public class Circle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input r");
+		int r = sc.nextInt();
+		double S = Math.PI * Math.pow(r, 2);
+		System.out.println("S = " + S);
+		
+		
+
+	}
+
+}
--- src/com/gmail/arsenycholexandra/Triangle.java
+++ src/com/gmail/arsenycholexandra/Triangle.java
@@ -4,7 +4,12 @@ public class Triangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 1;
+		int a = 3;
+		int b = 4;
+		int c = 5;
+		double p = (a + b + c) / 2;
+		double S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		System.out.println(S);
 		
 	}
 
