067d794b652eceedb136baee2e97483cddb3dfd6
aarsenych <arsenycholexandra@gmail.com>
2018-06-07 17:20:16 +0300

    resolving problem

--- src/com/gmail/arsenycholexandra/Triangle.java
+++ src/com/gmail/arsenycholexandra/Triangle.java
@@ -4,18 +4,12 @@ public class Triangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
 		int a = 3;
 		int b = 4;
 		int c = 5;
 		double p = (a + b + c) / 2;
 		double S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
 		System.out.println(S);
-		
-=======
-		int a = 1;
-		int b = 3;
->>>>>>> e03d62d029a59f4f62bbcc1bb75628a2cb527c60
 	}
 
 }
