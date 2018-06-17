3405e82a00cda5f4cd05a2629f37e8b5369a0787
aarsenych <arsenycholexandra@gmail.com>
2018-06-17 16:36:43 +0300

    task1

--- src/com/gmail/arsenycholexandra/homework3/level2/Task1.java
+++ src/com/gmail/arsenycholexandra/homework3/level2/Task1.java
@@ -7,15 +7,15 @@ public class Task1 {
 	public static void main(String[] args) {
 		int width = 4;
 		int height = 2 * width -1;
+		int j = 1;
 		
-		for (int i = 1; i <= height; i++ ) {
-			if ( i <= width) {
-				System.out.print(String.join("", Collections.nCopies(i, "*")));
-			}
-			if ( i > width ) {
-				System.out.print(String.join("", Collections.nCopies(height + 1 - i, "*")));
-			}
-			System.out.println("");
+		for (int i = 1; i <= height; j++ ) {
+				System.out.print("*");
+				if (( i < width && j >= i ) || (i >= width && j > height - i)) {
+					System.out.println("");
+					i++;
+					j = 0;
+				}
 		}
 	}
 }
